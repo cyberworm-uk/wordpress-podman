7f33ad54edecc717a14cae2436efbcbf816fb6d2
kubeworm <93949496+guest42069@users.noreply.github.com>
2023-10-09 12:13:07 +0100

    Create wp-create-quadlet.sh

---
+++ wp-create-quadlet.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+DB_PASSWORD="$(head -c 32 /dev/urandom | base64 | tr -cd "[a-zA-Z0-9]")"
+PREFIX=${1:-$(head -c 8 /dev/urandom | hexdump -e'"%x"')}
+
+printf "%s" "${DB_PASSWORD}" | podman secret create "${PREFIX}" -
+
+SQL_CONTAINER="[Unit]
+Description=${PREFIX} SQL container
+
+[Container]
+Image=docker.io/library/mariadb:latest
+AutoUpdate=registry
+
+Volume=${PREFIX}-mariadb.volume:/var/lib/mysql
+PodmanArgs=--secret=${PREFIX}
+Network=${PREFIX}.network
+
+Environment=MARIADB_ROOT_PASSWORD_FILE=/run/secrets/${PREFIX}
+Environment=MARIADB_DATABASE=blog
+
+[Service]
+Restart=always
+TimeoutStartSec=900
+
+[Install]
+WantedBy=default.target"
+
+WORDPRESS_CONTAINER="[Unit]
+Description=${PREFIX} Wordpress container
+
+[Container]
+Image=docker.io/library/wordpress:fpm-alpine
+AutoUpdate=registry
+
+Volume=${PREFIX}-wordpress.volume:/var/www/html
+PodmanArgs=--secret=${PREFIX}
+Network=${PREFIX}.network
+
+Environment=WORDPRESS_DB_HOST=systemd-${PREFIX}-sql
+Environment=WORDPRESS_DB_USER=root
+Environment=WORDPRESS_DB_PASSWORD_FILE=/run/secrets/${PREFIX}
+Environment=WORDPRESS_DB_NAME=blog
+
+[Service]
+Restart=always
+TimeoutStartSec=900
+
+[Install]
+WantedBy=default.target"
+
+NGINX_CONTAINER="[Unit]
+Description=${PREFIX} Nginx container
+
+[Container]
+Image=docker.io/library/nginx:alpine
+AutoUpdate=registry
+
+Volume=${PREFIX}-wordpress.volume:/var/www/html
+Volume=${PREFIX}-nginx.volume:/etc/nginx/conf.d
+Network=${PREFIX}.network
+
+Environment=MARIADB_ROOT_PASSWORD_FILE=/run/secrets/${PREFIX}
+Environment=MARIADB_DATABASE=blog
+
+[Service]
+Restart=always
+TimeoutStartSec=900
+
+[Install]
+WantedBy=default.target"
+
+NGINX_CONFIG="server {
+  listen 80;
+  listen [::]:80;
+  root /var/www/html;
+  index index.php;
+  server_name _;
+  server_tokens off;
+  location / {
+    try_files \$uri \$uri/ /index.php?\$args;
+  }
+  location ~ \.php$ {
+    fastcgi_split_path_info ^(.+\.php)(/.+)$;
+    fastcgi_pass systemd-${PREFIX}-wordpress:9000;
+    fastcgi_index index.php;
+    include fastcgi_params;
+    fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+    fastcgi_param SCRIPT_NAME \$fastcgi_script_name;
+  }
+}"
+
+NETWORK_CONFIG="[Network]
+IPv6=true"
+
+podman volume create "systemd-${PREFIX}-nginx"
+
+echo "${SQL_CONTAINER}" > "${PREFIX}-sql.container"
+echo "${WORDPRESS_CONTAINER}" > "${PREFIX}-wordpress.container"
+echo "${NGINX_CONTAINER}" > "${PREFIX}-nginx.container"
+echo "${NGINX_CONFIG}" > $(podman volume inspect -f '{{ .Mountpoint }}' "systemd-${PREFIX}-nginx")/blog.conf
+echo "[Volume]" | tee "${PREFIX}-mariadb.volume" | tee "${PREFIX}-wordpress.volume" | tee "${PREFIX}-nginx.volume"
+echo "${NETWORK_CONFIG}" > "${PREFIX}.network"
